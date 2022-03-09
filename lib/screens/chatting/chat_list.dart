f1545eab7170196b05a053ac5364d8a89af15bdb
yejun_1220@naver.com <yejun_1220@naver.com>
2022-03-09 11:49:04 +0900

    [DELETE] - url_launcher 삭제

--- lib/screens/chatting/chat_list.dart
+++ lib/screens/chatting/chat_list.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_app/manager/manager_info.dart';
 import 'package:flutter_app/screens/chatting/in_chat_screen.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
