f771ee9e10928deb1537b79fe5e0999d390f0be9
yejun_1220@naver.com <yejun_1220@naver.com>
2022-03-14 22:11:17 +0900

    [UPDATE] - 비밀전호 재설정
    resetUserEmail에 값이 안들어오는 문제가 있음.

---
+++ lib/screens/auth/password_reset.dart
@@ -0,0 +1,43 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class PasswordReset extends StatefulWidget {
+  final FirebaseAuth auth;
+  const PasswordReset({required this.auth, Key? key}) : super(key: key);
+
+  @override
+  State<PasswordReset> createState() => _PasswordResetState();
+}
+
+class _PasswordResetState extends State<PasswordReset> {
+  @override
+  Widget build(BuildContext context) {
+
+    String resetUserEmail = '';
+
+    return Scaffold(
+        body: Container(
+          child: Column(
+            children: [
+              TextField(
+                onChanged: (value) {
+                  setState(() {
+                    resetUserEmail = value.trim();
+                    print(resetUserEmail);
+                  });
+                }
+              ),
+
+              TextButton(
+                onPressed: () async{
+                  print(resetUserEmail);
+                  await widget.auth.setLanguageCode("ko");
+                  await widget.auth.sendPasswordResetEmail(email: resetUserEmail);
+                },
+                child: Text('재전송')
+              )
+            ],
+          ),
+        ));
+  }
+}
--- lib/screens/auth/sign_In.dart
+++ lib/screens/auth/sign_In.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app/screens/auth/password_reset.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_sign_in/google_sign_in.dart';
@@ -24,8 +25,7 @@ class _SignInState extends State<SignIn> {
     final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
 
     // Obtain the auth details from the request
-    final GoogleSignInAuthentication? googleAuth =
-    await googleUser?.authentication;
+    final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
 
     // Create a new credential
     final credential = GoogleAuthProvider.credential(
@@ -42,8 +42,7 @@ class _SignInState extends State<SignIn> {
     final LoginResult loginResult = await FacebookAuth.instance.login();
 
     // Create a credential from the access token
-    final OAuthCredential facebookAuthCredential =
-    FacebookAuthProvider.credential(loginResult.accessToken!.token);
+    final OAuthCredential facebookAuthCredential = FacebookAuthProvider.credential(loginResult.accessToken!.token);
 
     // Once signed in, return the UserCredential
     return FirebaseAuth.instance.signInWithCredential(facebookAuthCredential);
@@ -52,7 +51,7 @@ class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset : false,
+      resizeToAvoidBottomInset: false,
       body: Container(
         //      color: Colors.yellow,
         //
@@ -79,13 +78,7 @@ class _SignInState extends State<SignIn> {
               width: ScreenUtil().setWidth(150),
               height: ScreenUtil().setHeight(70),
               child: Center(
-                child: Text(
-                  "나와산책",
-                  style: TextStyle(
-                      fontFamily: "Montserrat",
-                      fontSize: 36.sp,
-                      fontWeight: FontWeight.bold),
-                ),
+                child: Text("나와산책", style: TextStyle(fontFamily: "Montserrat", fontSize: 36.sp, fontWeight: FontWeight.bold)),
               ),
             ),
             Container(
@@ -101,9 +94,9 @@ class _SignInState extends State<SignIn> {
               height: ScreenUtil().setHeight(40),
               child: Center(
                   child: Text(
-                "다양한 매력을 가진 매니저를 선택하고,",
-                style: TextStyle(fontFamily: "gangwon", fontSize: 18.sp),
-              )),
+                    "다양한 매력을 가진 매니저를 선택하고,",
+                    style: TextStyle(fontFamily: "gangwon", fontSize: 18.sp),
+                  )),
             ),
             Container(
               //           color: Colors.green,
@@ -116,20 +109,14 @@ class _SignInState extends State<SignIn> {
               //
               width: ScreenUtil().setWidth(380),
               height: ScreenUtil().setHeight(40),
-              child: Center(
-                  child: Text(
-                "산책을 시작해 보세요!",
-                style: TextStyle(fontFamily: "gangwon", fontSize: 18.sp),
-              )),
+              child: Center(child: Text("산책을 시작해 보세요!", style: TextStyle(fontFamily: "gangwon", fontSize: 18.sp))),
             ),
             Container(
               //         color: Colors.blue,
               //
               width: ScreenUtil().setWidth(327),
               height: ScreenUtil().setHeight(35),
-              child: Text("이메일",
-                  style:
-                      TextStyle(fontSize: 15.sp, fontWeight: FontWeight.bold)),
+              child: Text("이메일", style: TextStyle(fontSize: 15.sp, fontWeight: FontWeight.bold)),
             ),
             Container(
               //         color: Colors.green,
@@ -156,18 +143,14 @@ class _SignInState extends State<SignIn> {
             ),
             Container(
               //           color: Colors.green,
-              //
               width: ScreenUtil().setWidth(327),
               height: ScreenUtil().setHeight(5),
             ),
             Container(
               //          color: Colors.blue,
-              //
               width: ScreenUtil().setWidth(327),
               height: ScreenUtil().setHeight(25),
-              child: Text("비밀번호",
-                  style:
-                      TextStyle(fontSize: 15.sp, fontWeight: FontWeight.bold)),
+              child: Text("비밀번호", style: TextStyle(fontSize: 15.sp, fontWeight: FontWeight.bold)),
             ),
             Container(
               //         color: Colors.green,
@@ -182,15 +165,16 @@ class _SignInState extends State<SignIn> {
               height: ScreenUtil().setHeight(48),
               child: TextField(
                 obscureText: true,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: '비밀번호 8~16자, 영문, 숫자, 특수문자',
-                  ),
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: '비밀번호 8~16자, 영문, 숫자, 특수문자',
+                ),
                 onChanged: (value) {
                   setState(() {
                     _userPassword = value.trim();
                   });
-                },),
+                },
+              ),
             ),
             Container(
               //          color: Colors.green,
@@ -214,9 +198,7 @@ class _SignInState extends State<SignIn> {
                   ),
                   IconButton(
                     iconSize: 24.sp,
-                    icon: _isPressed
-                        ? Icon(Icons.circle)
-                        : Icon(Icons.circle_outlined),
+                    icon: _isPressed ? Icon(Icons.circle) : Icon(Icons.circle_outlined),
                     color: _isPressed ? Colors.grey : Colors.grey,
                     onPressed: () {
                       setState(() {
@@ -229,8 +211,7 @@ class _SignInState extends State<SignIn> {
                     //
                     child: Text(
                       "자동로그인",
-                      style:
-                          TextStyle(fontSize: 12.sp, color: Color(0xff231515)),
+                      style: TextStyle(fontSize: 12.sp, color: Color(0xff231515)),
                     ),
                   ),
                   Container(
@@ -243,8 +224,7 @@ class _SignInState extends State<SignIn> {
                     //
                     child: Text(
                       "개인정보처리방침",
-                      style:
-                          TextStyle(fontSize: 12.sp, color: Color(0xff351313)),
+                      style: TextStyle(fontSize: 12.sp, color: Color(0xff351313)),
                     ),
                   ),
                 ],
@@ -261,21 +241,17 @@ class _SignInState extends State<SignIn> {
                 height: ScreenUtil().setHeight(43),
                 child: MaterialButton(
                   color: Color(0xff74BABC),
-                  onPressed: () async{
-
+                  onPressed: () async {
                     try {
-                      final loginUser = await _auth.signInWithEmailAndPassword(
-                          email: _userEmail, password: _userPassword);
-                      if (loginUser.user != null)
-                        Navigator.pushNamed(context, '/home');
+                      final loginUser = await _auth.signInWithEmailAndPassword(email: _userEmail, password: _userPassword);
+                      if (loginUser.user != null) Navigator.pushNamed(context, '/home');
                     } catch (e) {
                       // 스냅바 구현 해야 함.
                     }
                   },
                   child: Text(
                     "로그인",
-                    style: TextStyle(
-                        color: Color(0xffffffff), fontWeight: FontWeight.bold),
+                    style: TextStyle(color: Color(0xffffffff), fontWeight: FontWeight.bold),
                   ),
                 )),
             Container(
@@ -300,17 +276,18 @@ class _SignInState extends State<SignIn> {
                   Container(
                     //        color: Colors.orange,
                     //
-                    child:
-                    // RaisedButton(
-                    //     child: Text("비밀번호 재설정"),
-                    //     onPressed: () async {
-                    //       await _auth.setLanguageCode("ko");
-                    //       _auth.sendPasswordResetEmail(email: _resetUserEmail);
-                    //     }),
-                    Text(
-                      "이메일 찾기 | 비밀번호 재설정",
-                      style:
-                          TextStyle(fontSize: 12.sp, color: Color(0xff666666)),
+                    child: TextButton(
+                      child: Text("이메일 찾기", style: TextStyle(fontSize: 12.sp, color: Color(0xff666666))),
+                      onPressed: () {},
+                    ),
+                  ),
+                  Container(
+                    child: TextButton(
+                      child: Text(" | 비밀번호 재설정", style: TextStyle(fontSize: 12.sp, color: Color(0xff666666))),
+                      onPressed: () {
+                        Navigator.of(context, rootNavigator: true)
+                            .push(MaterialPageRoute(builder: (context) => PasswordReset(auth: _auth)));
+                      },
                     ),
                   ),
                   Container(
@@ -367,10 +344,9 @@ class _SignInState extends State<SignIn> {
               height: ScreenUtil().setHeight(43),
               child: MaterialButton(
                 color: Color(0xffF6F2E9),
-                onPressed: () async{
+                onPressed: () async {
                   final loginUser = await signInWithGoogle();
-                  if(loginUser.user != null)
-                    Navigator.pushNamed(context, '/home');
+                  if (loginUser.user != null) Navigator.pushNamed(context, '/home');
                 },
                 child: Text("GOOGLE로 로그인하기"),
               ),
@@ -386,10 +362,9 @@ class _SignInState extends State<SignIn> {
                 height: ScreenUtil().setHeight(43),
                 child: MaterialButton(
                   color: Color(0xff3B5999),
-                  onPressed: () async{
+                  onPressed: () async {
                     final loginUser = await signInWithFacebook();
-                    if(loginUser.user != null)
-                      Navigator.pushNamed(context, '/home');
+                    if (loginUser.user != null) Navigator.pushNamed(context, '/home');
                   },
                   child: Text(
                     "FACEBOOK으로 로그인하기",
@@ -415,8 +390,8 @@ class _SignInState extends State<SignIn> {
                   Container(
                     width: ScreenUtil().setWidth(130),
                     child: MaterialButton(
-                        //                                 color: Colors.red,
-                        //
+                      //                                 color: Colors.red,
+                      //
                         onPressed: () {
                           Navigator.pushNamed(context, '/signup');
                         },
