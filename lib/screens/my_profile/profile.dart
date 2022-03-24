86c35ec068f9beae2e903dd58e955d997424e81b
GSD-unity <shhsjk2002@gmail.com>
2022-03-24 13:17:45 +0900

    [Insert] 내프로필에 찜목록 추가

--- lib/screens/my_profile/profile.dart
+++ lib/screens/my_profile/profile.dart
@@ -1,6 +1,7 @@
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app/screens/wish_list/wish_list.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 
@@ -84,6 +85,17 @@ class Profile extends StatelessWidget {
                   '공지사항',
                   style: TextStyle(fontSize: 19.sp, color: Color(0xff878787), fontWeight: FontWeight.bold),
                 )),
+          ),
+          ListTile(
+            tileColor: Color(0xfff9f9f9),
+            onTap: () {Navigator.of(context, rootNavigator: true)
+                .push(MaterialPageRoute(builder: (_) => WishList()));},
+            leading: Icon(
+              EvaIcons.heartOutline,
+              size: 30,
+            ),
+            title: Transform.translate(
+                offset: Offset(-15, 0), child: Text('찜 목록', style: TextStyle(fontSize: 19.sp, color: Colors.grey, fontWeight: FontWeight.bold))),
           ),
           ListTile(
             tileColor: Color(0xfff9f9f9),
