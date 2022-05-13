e919377d8b7ea67d287c58309df7a2be1fb17aef
yejun_1220@naver.com <yejun_1220@naver.com>
2022-05-13 18:45:56 +0900

    [Insert] 게시판 댓글 기능 구현
    사용자 UID, 댓글 내용

---
+++ lib/screens/community/write_comment.dart
@@ -0,0 +1,46 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import '../freeboard/freeboard.dart';
+
+class WriteComment extends StatelessWidget {
+  final String boardNum;
+
+  const WriteComment(this.boardNum, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    String text = '';
+    final _currentUser = FirebaseAuth.instance.currentUser;
+
+    return Scaffold(
+      appBar: AppBar(title: Text("댓글 스크린")),
+      body: Container(
+          child: Column(children: [
+            TextField(
+              onChanged: (value) {
+                text = value.trim();
+              },
+            ),
+            TextButton(
+                onPressed: () async {
+                  var sn = await FirebaseFirestore.instance.collection('board_test').doc(boardNum).get();
+
+                  await FirebaseFirestore.instance.collection('board_test').doc(boardNum).collection('comment').add({
+                    'userUID': _currentUser!.uid,
+                    'text': text,
+                  });
+
+                  FirebaseFirestore.instance.collection('board_test').doc(boardNum).update({
+                    'comments': sn['comments'] +1
+                  });
+
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => FreeBoard()));
+                },
+                child: Text("작성"))
+          ])),
+    );
+  }
+}
