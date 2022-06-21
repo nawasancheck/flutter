2fb4113a5ca3b58e2572993ac05948355c3dacf8
yejun_1220@naver.com <yejun_1220@naver.com>
2022-06-21 15:42:53 +0900

    [Update] 프리보드 인페이지 최적화

--- lib/screens/freeboard/freeboard_content.dart
+++ lib/screens/freeboard/freeboard_content.dart
@@ -1,25 +1,33 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import '../../logic/freeboard/write/write_comment.dart';
+
 class FreeBoardContent extends StatelessWidget {
-  const FreeBoardContent({Key? key}) : super(key: key);
+  final String boardNum;
+
+  const FreeBoardContent(this.boardNum, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
-        iconTheme: IconThemeData(color: Color(0xff324755),),
-        title: Text("자유게시판",
-            style: TextStyle(
-              color: Color(0xff324755),
-              fontSize: 20.sp,
-              fontWeight: FontWeight.bold,
-            )),
+        iconTheme: IconThemeData(
+          color: Color(0xff324755),
+        ),
+        title: Text(
+          "자유게시판",
+          style: TextStyle(
+            color: Color(0xff324755),
+            fontSize: 20.sp,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
         centerTitle: true,
         backgroundColor: Color(0xffffffff),
-
         actions: [
           IconButton(
             icon: Icon(
@@ -27,238 +35,217 @@ class FreeBoardContent extends StatelessWidget {
               color: Color(0xff324755),
               size: 20.16.sp,
             ),
-            onPressed: () {
-            },
-          )
+            onPressed: () {},
+          ),
         ],
       ),
-      body: Container(
-        color: Color(0xffececec),
-        //color: Colors.yellow,
-        child: Column(children: [
-          Container(
-            color: Color(0xffe1f3f3),
-            //         color: Colors.green,
-            width: ScreenUtil().screenWidth,
-            height: ScreenUtil().setHeight(80),
-            child: Center(
-              child: Container(
-                // color: Color(0xffe1f3f3),
-                //              color: Colors.blue,
-                width: ScreenUtil().setWidth(350),
-                height: ScreenUtil().setHeight(80),
-                child: Row(
-                  children: [
-                    Container(
-                      width: ScreenUtil().setWidth(50),
-                      height: ScreenUtil().setHeight(50),
-                      decoration: BoxDecoration(
-                          color: Colors.grey[350],
-                          borderRadius: BorderRadius.circular(10)
-                      ),),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
+      body: FutureBuilder(
+        future: FirebaseFirestore.instance.collection('board_test').doc(boardNum).get(),
+        builder: (context, AsyncSnapshot<DocumentSnapshot<Map<String, dynamic>>> snapshot) {
+          if (!snapshot.hasData) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          var docs = snapshot.data!;
+
+          return Container(
+            color: Color(0xffececec),
+            //color: Colors.yellow,
+            child: Column(
+              children: [
+                Container(
+                  color: Color(0xffe1f3f3),
+                  //         color: Colors.green,
+                  width: ScreenUtil().screenWidth,
+                  height: ScreenUtil().setHeight(80),
+                  child: Center(
+                    child: Container(
+                      // color: Color(0xffe1f3f3),
+                      //              color: Colors.blue,
+                      width: ScreenUtil().setWidth(350),
+                      height: ScreenUtil().setHeight(80),
+                      child: Row(
+                        children: [
+                          Container(
+                            width: ScreenUtil().setWidth(50),
+                            height: ScreenUtil().setHeight(50),
+                            decoration: BoxDecoration(color: Colors.grey[350], borderRadius: BorderRadius.circular(10)),
+                          ),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Row(
+                                children: [
+                                  Text("${docs['userName']}"),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Text(" ${docs['time'].toDate()}"),
+                                ],
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                WriteComment(boardNum),
+                Container(
+                  //           color: Colors.blue,
+                  width: ScreenUtil().setWidth(350),
+                  height: ScreenUtil().setHeight(50),
+                  child: Text(
+                    "${docs['title']}",
+                    style: TextStyle(fontSize: 20.sp, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Flexible(
+                  fit: FlexFit.loose,
+                  child: Container(
+                    //          color: Colors.green,
+                    width: ScreenUtil().setWidth(350),
+                    height: ScreenUtil().setHeight(200),
+                    child: Text(
+                      '${docs['content']}',
+                      style: TextStyle(
+                        color: Color(0xff8e8594),
+                        fontSize: 18.sp,
+                      ),
+                    ),
+                  ),
+                ),
+                FutureBuilder(
+                  future: FirebaseFirestore.instance.collection('board_test').doc(boardNum).collection('comment').get(),
+                  builder: (context, AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot2) {
+                    if (!snapshot2.hasData) {
+                      return Center(child: CircularProgressIndicator());
+                    }
+
+                    if (snapshot2.connectionState == ConnectionState.waiting) {
+                      return Center(child: CircularProgressIndicator());
+                    }
+
+                    var docs2 = snapshot2.data!.docs;
+
+                    return Column(
                       children: [
-                        Row(
-                          children: [
-                            Text("익명 or ID"),
-                          ],
+                        Container(
+                          color: Colors.grey,
+                          width: ScreenUtil().setWidth(350),
+                          height: ScreenUtil().setHeight(30),
+                          child: Row(
+                            children: [
+                              Icon(
+                                EvaIcons.heartOutline,
+                                color: Colors.redAccent,
+                              ),
+                              Text(
+                                " ${docs['isPressedList'].length}  ",
+                                style: TextStyle(
+                                  fontSize: 15.sp,
+                                  color: Colors.redAccent,
+                                ),
+                              ),
+                              Icon(
+                                EvaIcons.messageCircleOutline,
+                                color: Colors.blue,
+                              ),
+                              Text(
+                                " ${docs2.length}  ",
+                                style: TextStyle(
+                                  fontSize: 15.sp,
+                                  color: Colors.blue,
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
-                        Row(
-                          children: [
-                            Text(" 05/11 08:37"),
-                          ],
-                        )
+                        ListView.builder(
+                          shrinkWrap: true,
+                          itemCount: docs2.length,
+                          itemBuilder: (context, index) {
+                            return Container(
+                              //        color: Colors.blue,
+                              width: ScreenUtil().setWidth(350),
+                              height: ScreenUtil().setHeight(120),
+                              child: Column(
+                                children: [
+                                  Row(
+                                    children: [
+                                      Container(
+                                        width: ScreenUtil().setWidth(30),
+                                        height: ScreenUtil().setHeight(30),
+                                        decoration: BoxDecoration(color: Colors.grey[350], borderRadius: BorderRadius.circular(10)),
+                                      ),
+                                      Text(" ${docs2[index]['userName']}"),
+                                      Flexible(fit: FlexFit.tight, child: Container()),
+                                      Container(
+                                        decoration: BoxDecoration(color: Color(0xffe1f3f3), borderRadius: BorderRadius.circular(3)),
+                                        child: Row(
+                                          children: [
+                                            Icon(
+                                              EvaIcons.messageCircleOutline,
+                                              color: Colors.grey,
+                                            ),
+                                            Text(
+                                              "  |  ",
+                                              style: TextStyle(
+                                                color: Colors.grey,
+                                              ),
+                                            ),
+                                            Icon(
+                                              EvaIcons.heartOutline,
+                                              color: Colors.grey,
+                                            ),
+                                            Text(
+                                              "  |  ",
+                                              style: TextStyle(
+                                                color: Colors.grey,
+                                              ),
+                                            ),
+                                            Icon(
+                                              EvaIcons.moreVertical,
+                                              color: Colors.grey,
+                                            )
+                                          ],
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  Row(
+                                    children: [
+                                      Text("\n${docs2[index]['text']}"),
+                                    ],
+                                  ),
+                                  Row(
+                                    children: [
+                                      Text("\n ${docs2[index]['time'].toDate()}"),
+                                    ],
+                                  ),
+                                  Divider(
+                                    //indent: 20, endIndent: 20,
+                                    thickness: 2,
+                                  )
+                                ],
+                              ),
+                            );
+                          },
+                        ),
                       ],
-                    ),
-                  ],
+                    );
+                  },
                 ),
-              ),
-            ),
-          ),
 
-          Container(
-            //           color: Colors.blue,
-            width: ScreenUtil().setWidth(350),
-            height: ScreenUtil().setHeight(50),
-            child: Text(
-              "제목",
-              style: TextStyle(
-                  fontSize: 20.sp,
-                  fontWeight: FontWeight.bold),
-            ),
-          ),
-
-          Flexible(
-            fit: FlexFit.loose,
-            child: Container(
-              //          color: Colors.green,
-              width: ScreenUtil().setWidth(350),
-              height: ScreenUtil().setHeight(200),
-              child: Text(
-                "내용입니당. 12341234123412341234123412341234123412341234123412341234123412341234가나다라마바사아자차카타파하",
-                style: TextStyle(
-                  color: Color(0xff8e8594),
-                  fontSize: 18.sp,
-                ),
-              ),
-            ),
-          ),
-
-          Container(
-              color: Colors.grey,
-              width: ScreenUtil().setWidth(350),
-              height: ScreenUtil().setHeight(30),
-              child: Row(children: [
-                Icon(
-                  EvaIcons.heartOutline,
-                  color: Colors.redAccent,
-                ),
-                Text(
-                  " 3  ",
-                  style: TextStyle(
-                    fontSize: 15.sp,
-                    color: Colors.redAccent,
-                  ),
-                ),
-                Icon(
-                  EvaIcons.messageCircleOutline,
-                  color: Colors.blue,
-                ),
-                Text(
-                  " 3  ",
-                  style: TextStyle(
-                    fontSize: 15.sp,
-                    color: Colors.blue,
-                  ),
-                ),
-              ],)
-          ),
-
-          Container(
-            //        color: Colors.blue,
-            width: ScreenUtil().setWidth(350),
-            height: ScreenUtil().setHeight(120),
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    Container(
-                      width: ScreenUtil().setWidth(30),
-                      height: ScreenUtil().setHeight(30),
-                      decoration: BoxDecoration(
-                          color: Colors.grey[350],
-                          borderRadius: BorderRadius.circular(10)
-                      ),),
-                    Text(" 익명 or ID"),
-                    Flexible(fit: FlexFit.tight,child: Container()),
-                    Container(
-                      decoration: BoxDecoration(
-                          color: Color(0xffe1f3f3),
-                          borderRadius: BorderRadius.circular(3)
-                      ),
-                      child: Row(children: [Icon(
-                        EvaIcons.messageCircleOutline,
-                        color: Colors.grey,
-                      ),
-                        Text("  |  ",style: TextStyle(color: Colors.grey,),),
-                        Icon(
-                          EvaIcons.heartOutline,
-                          color: Colors.grey,
-                        ),
-                        Text("  |  ",style: TextStyle(color: Colors.grey,),),
-                        Icon(EvaIcons.moreVertical,
-                          color: Colors.grey,)],),
-                    ),
-
-
-                  ],
-                ),
-                Row(
-                  children: [
-                    Text("\n댓글 내용~~"),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Text("\n 05/11 08:37"),
-                  ],
-                ),
-                Divider(//indent: 20, endIndent: 20,
-                  thickness: 2,)
               ],
             ),
-          ),
-          Container(
-            //     color: Colors.blue,
-            width: ScreenUtil().setWidth(350),
-            height: ScreenUtil().setHeight(120),
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    Container(
-                      width: ScreenUtil().setWidth(30),
-                      height: ScreenUtil().setHeight(30),
-                      decoration: BoxDecoration(
-                          color: Colors.grey[350],
-                          borderRadius: BorderRadius.circular(10)
-                      ),),
-                    Text(" 익명 or ID"),
-                    Flexible(fit: FlexFit.tight,child: Container()),
-                    Container(
-                      decoration: BoxDecoration(
-                          color: Color(0xffe1f3f3),
-                          borderRadius: BorderRadius.circular(3)
-                      ),
-                      child: Row(children: [Icon(
-                        EvaIcons.messageCircleOutline,
-                        color: Colors.grey,
-                      ),
-                        Text("  |  ",style: TextStyle(color: Colors.grey,),),
-                        Icon(
-                          EvaIcons.heartOutline,
-                          color: Colors.grey,
-                        ),
-                        Text("  |  ",style: TextStyle(color: Colors.grey,),),
-                        Icon(EvaIcons.moreVertical,
-                          color: Colors.grey,)],),
-                    ),
-
-
-                  ],
-                ),
-                Row(
-                  children: [
-                    Text("\n댓글 내용~~"),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Text("\n 05/11 08:37"),
-                  ],
-                ),
-                Divider(//indent: 20, endIndent: 20,
-                  thickness: 2,)
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: '댓글을 입력해주세요',
-                labelStyle: TextStyle(
-                  fontSize: 14.sp,
-                  color: Color(0xffc4c4c4),
-                ),
-              ),
-              onChanged: (value) {
-              },
-            ),
-          ),
-        ],),
+          );
+        },
       ),
     );
   }
