ced2512d10164628b71a9a195535a4abe8e09ca9
yejun_1220@naver.com <yejun_1220@naver.com>
2022-06-27 01:23:30 +0900

    [Clean] 코드 정리

--- lib/screens/auth/sign_In.dart
+++ lib/screens/auth/sign_In.dart
@@ -25,8 +25,7 @@ class _SignInState extends State<SignIn> {
     final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
 
     // Obtain the auth details from the request
-    final GoogleSignInAuthentication? googleAuth =
-        await googleUser?.authentication;
+    final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
 
     // Create a new credential
     final credential = GoogleAuthProvider.credential(
@@ -43,8 +42,7 @@ class _SignInState extends State<SignIn> {
     final LoginResult loginResult = await FacebookAuth.instance.login();
 
     // Create a credential from the access token
-    final OAuthCredential facebookAuthCredential =
-        FacebookAuthProvider.credential(loginResult.accessToken!.token);
+    final OAuthCredential facebookAuthCredential = FacebookAuthProvider.credential(loginResult.accessToken!.token);
 
     // Once signed in, return the UserCredential
     return FirebaseAuth.instance.signInWithCredential(facebookAuthCredential);
@@ -56,7 +54,9 @@ class _SignInState extends State<SignIn> {
       resizeToAvoidBottomInset: false,
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(0),
-        child: AppBar(backgroundColor: Colors.transparent, elevation: 0,
+        child: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
         ),
       ),
       body: Container(
@@ -66,34 +66,24 @@ class _SignInState extends State<SignIn> {
 
         child: Column(
           children: [
-
             Padding(
               padding: const EdgeInsets.fromLTRB(0, 50, 0, 30),
               child: Center(
-                child: Text("나와산책",
-                    style: TextStyle(
-                        color: Color(0xff000000),
-                        fontSize: 32.sp,
-                        fontWeight: FontWeight.bold)),
+                child: Text("나와산책", style: TextStyle(color: Color(0xff000000), fontSize: 32.sp, fontWeight: FontWeight.bold)),
               ),
             ),
-
             Padding(
               padding: const EdgeInsets.fromLTRB(0, 0, 0, 3),
               child: Container(
-          //             color: Colors.blue,
+                //             color: Colors.blue,
                 width: 327.w,
                 height: 25.h,
                 child: Text(
                   "이메일",
-                  style: TextStyle(
-                      color: Color(0xff000000),
-                      fontSize: 16.sp,
-                      fontWeight: FontWeight.bold),
+                  style: TextStyle(color: Color(0xff000000), fontSize: 16.sp, fontWeight: FontWeight.bold),
                 ),
               ),
             ),
-
             Container(
               color: Colors.white,
               width: ScreenUtil().setWidth(327),
@@ -114,21 +104,15 @@ class _SignInState extends State<SignIn> {
                 },
               ),
             ),
-
             Padding(
               padding: const EdgeInsets.fromLTRB(0, 3, 0, 3),
               child: Container(
-             //             color: Colors.blue,
+                //             color: Colors.blue,
                 width: 327.w,
                 height: 25.h,
-                child: Text("비밀번호",
-                    style: TextStyle(
-                        color: Color(0xff000000),
-                        fontSize: 16.sp,
-                        fontWeight: FontWeight.bold)),
+                child: Text("비밀번호", style: TextStyle(color: Color(0xff000000), fontSize: 16.sp, fontWeight: FontWeight.bold)),
               ),
             ),
-
             Container(
               color: Colors.white,
               width: ScreenUtil().setWidth(327),
@@ -150,18 +134,24 @@ class _SignInState extends State<SignIn> {
                 },
               ),
             ),
-
             Padding(
               padding: const EdgeInsets.fromLTRB(0, 0, 0, 3),
               child: Container(
-               //         color: Colors.green,
+                //         color: Colors.green,
                 width: 327.w,
                 height: 40.h,
                 child: Row(
                   children: [
                     InkWell(
-                      child: _isPressed ? Icon(Icons.circle, color: Color(0xffc4c4c4),)
-                          : Icon(Icons.circle_outlined, color: Color(0xffc4c4c4),),
+                      child: _isPressed
+                          ? Icon(
+                              Icons.circle,
+                              color: Color(0xffc4c4c4),
+                            )
+                          : Icon(
+                              Icons.circle_outlined,
+                              color: Color(0xffc4c4c4),
+                            ),
                       onTap: () {
                         setState(() {
                           _isPressed = !_isPressed;
@@ -169,30 +159,27 @@ class _SignInState extends State<SignIn> {
                       },
                     ),
                     Container(
-                 //         color: Colors.red,
+                      //         color: Colors.red,
                       width: 5.w,
                     ),
                     Text(
                       "자동로그인",
-                      style:
-                          TextStyle(fontSize: 12.sp, color: Color(0xff231515)),
+                      style: TextStyle(fontSize: 12.sp, color: Color(0xff231515)),
                     ),
                     Flexible(
                       fit: FlexFit.tight,
                       child: Container(
-                   //                              color: Colors.red,
+                          //                              color: Colors.red,
                           ),
                     ),
                     Text(
                       "개인정보처리방침",
-                      style:
-                          TextStyle(fontSize: 12.sp, color: Color(0xff351313)),
+                      style: TextStyle(fontSize: 12.sp, color: Color(0xff351313)),
                     ),
                   ],
                 ),
               ),
             ),
-
             Padding(
               padding: const EdgeInsets.fromLTRB(0, 20, 0, 3),
               child: Container(
@@ -202,26 +189,21 @@ class _SignInState extends State<SignIn> {
                   onPressed: () async {
                     try {
                       final loginUser = await _auth.signInWithEmailAndPassword(email: _userEmail, password: _userPassword);
-                      FirebaseAuth.instance.setPersistence(Persistence.NONE);} catch (e) {
+                    } catch (e) {
                       // 스냅바 구현 해야 함.
                     }
                   },
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10)),
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
                   color: Color(0xff74BABC),
                   child: Text(
                     "로그인",
-                    style: TextStyle(
-                        fontSize: 16.sp,
-                        color: Color(0xffffffff),
-                        fontWeight: FontWeight.bold),
+                    style: TextStyle(fontSize: 16.sp, color: Color(0xffffffff), fontWeight: FontWeight.bold),
                   ),
                 ),
               ),
             ),
-
             Container(
-                    //      color: Colors.orange,
+              //      color: Colors.orange,
               width: 327.w,
               height: 32.h,
               child: Row(
@@ -230,17 +212,15 @@ class _SignInState extends State<SignIn> {
                   Flexible(
                     fit: FlexFit.tight,
                     child: Container(
-                      //               color: Colors.red,
+                        //               color: Colors.red,
                         width: 50.w),
                   ),
                   Container(
                     height: 32.h,
                     width: 80.w,
-                       //     color: Colors.brown,
+                    //     color: Colors.brown,
                     child: TextButton(
-                      child: Text("이메일 찾기",
-                          style: TextStyle(
-                              fontSize: 13.sp, color: Color(0xff666666))),
+                      child: Text("이메일 찾기", style: TextStyle(fontSize: 13.sp, color: Color(0xff666666))),
                       onPressed: () {},
                     ),
                   ),
@@ -254,24 +234,19 @@ class _SignInState extends State<SignIn> {
                     width: 110.w,
                     // color: Colors.purple,
                     child: TextButton(
-                      child: Text("비밀번호 재설정",
-                          style: TextStyle(
-                              fontSize: 13.sp, color: Color(0xff666666))),
+                      child: Text("비밀번호 재설정", style: TextStyle(fontSize: 13.sp, color: Color(0xff666666))),
                       onPressed: () {
-                        Navigator.of(context, rootNavigator: true).push(
-                            MaterialPageRoute(
-                                builder: (context) =>
-                                    PasswordReset(auth: _auth)));
+                        Navigator.of(context, rootNavigator: true).push(MaterialPageRoute(builder: (context) => PasswordReset(auth: _auth)));
                       },
                     ),
                   ),
                 ],
               ),
             ),
-
-            Padding(padding: const EdgeInsets.fromLTRB(0, 20, 0, 3),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 20, 0, 3),
               child: Container(
-                      //   color: Colors.blue,
+                //   color: Colors.blue,
                 width: 327.w,
                 height: 40.h,
                 child: Row(
@@ -297,7 +272,6 @@ class _SignInState extends State<SignIn> {
                 ),
               ),
             ),
-
             Padding(
               padding: const EdgeInsets.fromLTRB(0, 0, 0, 15),
               child: Container(
@@ -306,23 +280,17 @@ class _SignInState extends State<SignIn> {
                 child: FlatButton(
                   onPressed: () async {
                     final loginUser = await signInWithGoogle();
-                    if (loginUser.user != null)
-                      Navigator.pushNamed(context, '/home');
+                    if (loginUser.user != null) Navigator.pushNamed(context, '/home');
                   },
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10)),
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
                   color: Color(0xffF6F2E9),
                   child: Text(
                     "GOOGLE로 로그인하기",
-                    style: TextStyle(
-                        fontSize: 13.sp,
-                        color: Color(0xff000000),
-                        fontWeight: FontWeight.bold),
+                    style: TextStyle(fontSize: 13.sp, color: Color(0xff000000), fontWeight: FontWeight.bold),
                   ),
                 ),
               ),
             ),
-
             Padding(
               padding: const EdgeInsets.fromLTRB(0, 0, 0, 15),
               child: Container(
@@ -331,23 +299,17 @@ class _SignInState extends State<SignIn> {
                 child: FlatButton(
                   onPressed: () async {
                     final loginUser = await signInWithFacebook();
-                    if (loginUser.user != null)
-                      Navigator.pushNamed(context, '/home');
+                    if (loginUser.user != null) Navigator.pushNamed(context, '/home');
                   },
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10)),
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
                   color: Color(0xff3B5999),
                   child: Text(
                     "FACEBOOK으로 로그인하기",
-                    style: TextStyle(
-                        fontSize: 13.sp,
-                        color: Color(0xffffffff),
-                        fontWeight: FontWeight.bold),
+                    style: TextStyle(fontSize: 13.sp, color: Color(0xffffffff), fontWeight: FontWeight.bold),
                   ),
                 ),
               ),
             ),
-
             Padding(
               padding: const EdgeInsets.fromLTRB(15, 0, 15, 3),
               child: Container(
@@ -359,8 +321,7 @@ class _SignInState extends State<SignIn> {
                       //                      color: Colors.orange,
                       child: Text(
                         "계정이 없으신가요?",
-                        style:
-                            TextStyle(fontSize: 15.sp, color: Color(0xff000000)),
+                        style: TextStyle(fontSize: 15.sp, color: Color(0xff000000)),
                       ),
                     ),
                     Container(
@@ -368,9 +329,7 @@ class _SignInState extends State<SignIn> {
                       width: 103.w,
                       height: 35,
                       child: TextButton(
-                        child: Text("회원가입 하기",
-                            style: TextStyle(
-                                fontSize: 15.sp, color: Color(0xff74babd))),
+                        child: Text("회원가입 하기", style: TextStyle(fontSize: 15.sp, color: Color(0xff74babd))),
                         onPressed: () {
                           Navigator.pushNamed(context, '/signup');
                         },
