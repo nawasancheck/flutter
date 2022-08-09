95d328417ffa3d206a5c0d0777f7256f00806c7c
김정우 <93897045+Kjgwo@users.noreply.github.com>
2022-08-09 19:09:09 +0900

    [Update] 더보기 페이지 & 요청사항 페이지

---
+++ lib/screens/my_profile/changed_name.dart
@@ -0,0 +1,99 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_app/screens/my_profile/my_profile.dart';
+import 'package:flutter_app/screens/my_profile/profile.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class ChangedName extends StatefulWidget {
+  const ChangedName({Key? key}) : super(key: key);
+
+  @override
+  State<ChangedName> createState() => _ChangedNameState();
+}
+
+class _ChangedNameState extends State<ChangedName> {
+  User? user = FirebaseAuth.instance.currentUser;
+  String name = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        title: Text(
+          "닉네임 변경",
+          style:
+              TextStyle(color: Color(0xff324755), fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(
+          color: Color(0xff324755),
+        ),
+        centerTitle: true,
+      ),
+      body: Container(
+        width: ScreenUtil().screenWidth,
+        height: ScreenUtil().screenHeight,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 50, 0, 30),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 0, 3),
+              child: Container(
+                width: 327.w,
+                height: 25.h,
+                child: Text(
+                  "닉네임 변경",
+                  style: TextStyle(color: Color(0xff000000), fontSize: 16.sp),
+                ),
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              width: ScreenUtil().setWidth(327),
+              height: ScreenUtil().setHeight(48),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelStyle: TextStyle(
+                    fontSize: 14.sp,
+                    color: Color(0xffc4c4c4),
+                  ),
+                  hintText: "변경할 닉네임을 입력하세요",
+                ),
+                onChanged: (value) {
+                  setState(() {
+                    name = value.trim();
+                  });
+                },
+              ),
+            ),
+            SizedBox(height: ScreenUtil().setHeight(100)),
+            Container(
+              width: ScreenUtil().setWidth(100),
+              height: ScreenUtil().setHeight(40),
+              child: MaterialButton(
+                color: Color(0xff74BABC),
+                onPressed: () {
+                  user?.updateDisplayName(name);
+                  Navigator.pushNamedAndRemoveUntil(context, '/', (_) => false);
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => Profile()));
+                },
+                child: Text(
+                  "변경",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
--- lib/screens/my_profile/profile.dart
+++ lib/screens/my_profile/profile.dart
@@ -2,7 +2,12 @@ import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/screens/my_profile/charge_point.dart';
+import 'package:flutter_app/screens/my_profile/customer_service.dart';
+import 'package:flutter_app/screens/my_profile/frequently_asked_questions.dart';
 import 'package:flutter_app/screens/my_profile/listener_application.dart';
+import 'package:flutter_app/screens/my_profile/listener_profile.dart';
+import 'package:flutter_app/screens/my_profile/my_profile.dart';
+import 'package:flutter_app/screens/my_profile/notice_page.dart';
 import 'package:flutter_app/screens/wish_list/wish_list.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -13,263 +18,174 @@ class Profile extends StatelessWidget {
   Widget build(BuildContext context) {
     final _user = FirebaseAuth.instance.currentUser;
     return Scaffold(
-      backgroundColor: Color(0xffffffff),
-      appBar: AppBar(
-        iconTheme: IconThemeData(color: Color(0xff324755),),
-        centerTitle: true,
-        elevation: 2.0,
-        toolbarHeight: 50,
-        backgroundColor: Colors.white,
-        title: Text(
-          "마이 페이지",
-          style: TextStyle(
-              fontSize: 20.sp,
-              color: Colors.black,
-              fontWeight: FontWeight.bold),
+        backgroundColor: Color(0xffffffff),
+        appBar: AppBar(
+          iconTheme: IconThemeData(color: Color(0xff324755),),
+          centerTitle: true,
+          elevation: 2.0,
+          toolbarHeight: 50,
+          backgroundColor: Colors.white,
+          title: Text(
+            "더보기",
+            style: TextStyle(
+                fontSize: 17.sp,
+                color: Colors.grey,
+                fontWeight: FontWeight.bold),
+          ),
         ),
-      ),
-      body: Container(
-        width: ScreenUtil().screenWidth,
-        height: ScreenUtil().screenHeight,
-        //color: Colors.greenAccent,
-        child: Column(children: [
-          Container(height: ScreenUtil().setHeight(20)),
-          Container(
-            //color: Colors.blue,
-            child: Row(children: [
-              Container(width: ScreenUtil().setWidth(5)),
-              Icon(EvaIcons.githubOutline, size: 100),
-              Container(width: ScreenUtil().setWidth(15)),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('${_user!.displayName}',
-                      style: TextStyle(
-                          fontSize: 23.sp,
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold)),
-                  Container(height: ScreenUtil().setHeight(5)),
-                  Text("${_user.email}",
-                      style: TextStyle(
-                        fontSize: 17.sp,
-                        color: Colors.grey,
-                      )),
-                ],
-              ),
-            ]),
-          ),
-          Container(
-            padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-            height: 60,
-            child: Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(5),
-              ),
-              color: Color(0xffe1f3f3),
-              child: Row(
-                children: [
-                  Container(width: ScreenUtil().setWidth(10)),
-                  Text("나와포인트 15,000",
-                      style: TextStyle(
-                          fontSize: 18.sp,
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold)),
-                  Container(width: ScreenUtil().setWidth(110)),
-                  TextButton(
-                      onPressed: () {
-                        Navigator.of(context, rootNavigator:true).push(MaterialPageRoute(
-                            builder:(_)=>ChargePoint()));
-                      },
-                      style: TextButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          backgroundColor: Colors.yellow,
-                          padding: EdgeInsets.fromLTRB(15, 0, 15, 2)),
-                      child: Text("충전하기",
+        body: ListView(children: [
+          Container(height: ScreenUtil().setHeight(10)),
+          ListView(
+            // padding: EdgeInsets.only(left: 5),
+            shrinkWrap: true,
+            children: ListTile.divideTiles(
+              color: Colors.black87,
+              context: context,
+              tiles: [
+                ListTile(
+                  tileColor: Color(0xffffffff),
+                  onTap: () {
+                    Navigator.of(context, rootNavigator: true).push(
+                        MaterialPageRoute(builder: (_) => MyProfile()));
+                  },
+                  leading: Icon(
+                    EvaIcons.person,
+                    size: 30,
+                    color: Color(0xfff5c462),
+                  ),
+                  trailing: Icon(
+                    EvaIcons.arrowIosForward,
+                    size: 25,
+                  ),
+                  title: Transform.translate(
+                    offset: Offset(-15, 0),
+                    child: Text('프로필',
+                        style: TextStyle(
+                            fontSize: 19.sp,
+                            color: Color(0xff878787),
+                            fontWeight: FontWeight.bold)),
+                  ),
+                ),
+                ListTile(
+                    tileColor: Color(0xff4472c4),
+                    onTap: () {
+                      Navigator.of(context, rootNavigator: true).push(
+                          MaterialPageRoute(builder: (_) => ListenerProfile()));
+                    },
+                    leading: Icon(
+                      EvaIcons.person,
+                      size: 30,
+                      color: Color(0xffffffff),
+                    ),
+                    trailing: Icon(
+                      EvaIcons.arrowIosForward,
+                      size: 25,
+                    ),
+                    title: Transform.translate(
+                        offset: Offset(-15, 0),
+                        child: Text('리스너 프로필관리',
+                            style: TextStyle(
+                                fontSize: 19.sp,
+                                color: Color(0xffffffff),
+                                fontWeight: FontWeight.bold)))
+                ),
+                ListTile(
+                    tileColor: Color(0xff4472c4),
+                    onTap: () {
+                      Navigator.of(context, rootNavigator: true).push(
+                          MaterialPageRoute(builder: (_) => WishList()));
+                    },
+                    leading: Icon(
+                      EvaIcons.heart,
+                      size: 30,
+                      color: Color(0xffffffff),
+                    ),
+                    trailing: Icon(
+                      EvaIcons.arrowIosForward,
+                      size: 25,
+                    ),
+                    title: Transform.translate(
+                        offset: Offset(-15, 0),
+                        child: Text('찜목록',
+                            style: TextStyle(
+                                fontSize: 19.sp,
+                                color: Color(0xffffffff),
+                                fontWeight: FontWeight.bold)))
+                ),
+                ListTile(
+                  tileColor: Color(0xffffffff),
+                  onTap: () {
+                    Navigator.of(context, rootNavigator: true).push(
+                        MaterialPageRoute(builder: (_) => CustomerService()));
+                  },
+                  leading: Icon(
+                    EvaIcons.phoneCall,
+                    size: 30,
+                    color: Color(0xfff5c462),
+                  ),
+                  trailing: Icon(
+                    EvaIcons.arrowIosForward,
+                    size: 25,
+                  ),
+                  title: Transform.translate(
+                      offset: Offset(-15, 0),
+                      child: Text('나와산책에 연락하기',
                           style: TextStyle(
-                              fontSize: 16.sp,
-                              color: Colors.black,
-                              fontWeight: FontWeight.bold)))
-                ],),
-            ),
+                              fontSize: 19.sp,
+                              color: Color(0xff878787),
+                              fontWeight: FontWeight.bold))),
+                ),
+                ListTile(
+                  tileColor: Color(0xffffffff),
+                  onTap: () {
+                    Navigator.of(context, rootNavigator: true).push(
+                        MaterialPageRoute(builder: (_) => NoticePage()));
+                  },
+                  leading: Icon(
+                    EvaIcons.bulb,
+                    size: 30,
+                    color: Color(0xfff5c462),
+                  ),
+                  trailing: Icon(
+                    EvaIcons.arrowIosForward,
+                    size: 25,
+                  ),
+                  title: Transform.translate(
+                      offset: Offset(-15, 0),
+                      child: Text('공지사항',
+                          style: TextStyle(
+                              fontSize: 19.sp,
+                              color: Color(0xff878787),
+                              fontWeight: FontWeight.bold))),
+                ),
+                ListTile(
+                  tileColor: Color(0xffffffff),
+                  onTap: () {
+                    Navigator.of(context, rootNavigator: true).push(
+                        MaterialPageRoute(
+                            builder: (_) => FrequentlyAskedQuestions()));
+                  },
+                  leading: Icon(
+                    EvaIcons.questionMarkCircle,
+                    size: 30,
+                    color: Color(0xfff5c462),
+                  ),
+                  trailing: Icon(
+                    EvaIcons.arrowIosForward,
+                    size: 25,
+                  ),
+                  title: Transform.translate(
+                      offset: Offset(-15, 0),
+                      child: Text('자주묻는 질문',
+                          style: TextStyle(
+                              fontSize: 19.sp,
+                              color: Color(0xff878787),
+                              fontWeight: FontWeight.bold))),
+                ),
+              ],
+            ).toList(),
           ),
-          Container(height: ScreenUtil().setHeight(13)),
-          Container(
-            width: ScreenUtil().screenWidth,
-            height: ScreenUtil().setHeight(447),
-            //color: Colors.orange,
-            child: Scrollbar(
-              child: ListView(
-                  padding: EdgeInsets.only(left: 5),
-                  shrinkWrap: true,
-                  children: <Widget>[
-                    Container(
-                      color: Color(0xfff9f9f9),
-                      width: ScreenUtil().screenWidth,
-                      height: ScreenUtil().setHeight(60),
-                      child: InkWell(
-                        onTap: () {
-                          Navigator.of(context, rootNavigator: true).push(
-                              MaterialPageRoute(
-                                  builder: (_) => ListenerApplication()));
-                        },
-                        child: Row(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.fromLTRB(15, 0, 10, 0),
-                              child: Icon(
-                                EvaIcons.smilingFaceOutline,
-                                size: 30,
-                                color: Color(0xff878787),
-                              ),
-                            ),
-                            Text(
-                              '리스너 지원하기',
-                              style: TextStyle(
-                                  fontSize: 19.sp,
-                                  color: Color(0xff878787),
-                                  fontWeight: FontWeight.bold),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    Container(
-                      color: Color(0xfff9f9f9),
-                      width: ScreenUtil().screenWidth,
-                      height: ScreenUtil().setHeight(60),
-                      child: InkWell(
-                        onTap: () {},
-                        child: Row(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.fromLTRB(15, 0, 10, 0),
-                              child: Icon(
-                                EvaIcons.bulbOutline,
-                                size: 30,
-                                color: Color(0xff878787),
-                              ),
-                            ),
-                            Text(
-                              '공지사항',
-                              style: TextStyle(
-                                  fontSize: 19.sp,
-                                  color: Color(0xff878787),
-                                  fontWeight: FontWeight.bold),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    Container(
-                      color: Color(0xfff9f9f9),
-                      width: ScreenUtil().screenWidth,
-                      height: ScreenUtil().setHeight(60),
-                      child: InkWell(
-                        onTap: () {
-                          Navigator.of(context, rootNavigator: true).push(
-                              MaterialPageRoute(builder: (_) => WishList()));
-                        },
-                        child: Row(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.fromLTRB(15, 0, 10, 0),
-                              child: Icon(
-                                EvaIcons.heartOutline,
-                                size: 30,
-                                color: Color(0xff878787),
-                              ),
-                            ),
-                            Text('찜 목록',
-                                style: TextStyle(
-                                    fontSize: 19.sp,
-                                    color: Colors.grey,
-                                    fontWeight: FontWeight.bold)),
-                          ],
-                        ),
-                      ),
-                    ),
-                    Container(
-                      color: Color(0xfff9f9f9),
-                      width: ScreenUtil().screenWidth,
-                      height: ScreenUtil().setHeight(60),
-                      child: InkWell(
-                        onTap: () {},
-                        child: Row(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.fromLTRB(15, 0, 10, 0),
-                              child: Icon(
-                                EvaIcons.messageCircleOutline,
-                                size: 30,
-                                color: Color(0xff878787),
-                              ),
-                            ),
-                            Text('고객센터',
-                                style: TextStyle(
-                                    fontSize: 19.sp,
-                                    color: Colors.grey,
-                                    fontWeight: FontWeight.bold)),
-                          ],
-                        ),
-                      ),
-                    ),
-                    Container(
-                      color: Color(0xfff9f9f9),
-                      width: ScreenUtil().screenWidth,
-                      height: ScreenUtil().setHeight(60),
-                      child: InkWell(
-                        onTap: () {},
-                        child: Row(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.fromLTRB(15, 0, 10, 0),
-                              child: Icon(
-                                EvaIcons.menu2Outline,
-                                size: 30,
-                                color: Color(0xff878787),
-                              ),
-                            ),
-                            Text('회원정보 변경',
-                                style: TextStyle(
-                                    fontSize: 19.sp,
-                                    color: Colors.grey,
-                                    fontWeight: FontWeight.bold)),
-                          ],
-                        ),
-                      ),
-                    ),
-                    Container(
-                      color: Color(0xfff9f9f9),
-                      width: ScreenUtil().screenWidth,
-                      height: ScreenUtil().setHeight(60),
-                      child: InkWell(
-                        onTap: () {},
-                        child: Row(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.fromLTRB(15, 0, 10, 0),
-                              child: Icon(
-                                EvaIcons.settings2Outline,
-                                size: 30,
-                                color: Color(0xff878787),
-                              ),
-                            ),
-                            Text('설정',
-                                style: TextStyle(
-                                    fontSize: 19.sp,
-                                    color: Colors.grey,
-                                    fontWeight: FontWeight.bold)),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ]),
-            ),
-          ),
-        ]),
-      ),
+        ])
     );
   }
 }
