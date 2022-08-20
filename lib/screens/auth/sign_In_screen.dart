e13fb921755b4c5082bcf304b9672df19b045748
yejun_1220@naver.com <yejun_1220@naver.com>
2022-08-21 04:14:42 +0900

    [Fix] 페이스북 로그인 버그 수정

--- lib/screens/auth/sign_In_screen.dart
+++ lib/screens/auth/sign_In_screen.dart
@@ -31,69 +31,69 @@ class _SignInScreenState extends State<SignInScreen> {
         height: ScreenUtil().screenHeight,
         child: Column(
           children: [
-            Container(
-              color: Colors.white,
-              width: ScreenUtil().setWidth(327),
-              height: ScreenUtil().setHeight(48),
-              child: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: '이메일을 입력해주세요',
-                  labelStyle: TextStyle(
-                    fontSize: 14.sp,
-                    color: Color(0xffc4c4c4),
-                  ),
-                ),
-                onChanged: (value) {
-                  setState(() {
-                    _userEmail = value.trim();
-                  });
-                },
-              ),
-            ),
-            Container(
-              color: Colors.white,
-              width: ScreenUtil().setWidth(327),
-              height: ScreenUtil().setHeight(48),
-              child: TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelStyle: TextStyle(
-                    fontSize: 14.sp,
-                    color: Color(0xffc4c4c4),
-                  ),
-                  labelText: '비밀번호 8~16자, 영문, 숫자, 특수문자',
-                ),
-                onChanged: (value) {
-                  setState(() {
-                    _userPassword = value.trim();
-                  });
-                },
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 20, 0, 3),
-              child: Container(
-                width: 327.w,
-                height: 43.h,
-                child: FlatButton(
-                  onPressed: () async {
-                    try {
-                      await AuthController.instance.authentication.signInWithEmailAndPassword(email: _userEmail, password: _userPassword);
-                    } catch (e) {
-                      // 스냅바 구현 해야 함.
-                    }
-                  },
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                  color: Color(0xff74BABC),
-                  child: Text(
-                    "로그인",
-                    style: TextStyle(fontSize: 16.sp, color: Color(0xffffffff), fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ),
-            ),
+            // Container(
+            //   color: Colors.white,
+            //   width: ScreenUtil().setWidth(327),
+            //   height: ScreenUtil().setHeight(48),
+            //   child: TextField(
+            //     decoration: InputDecoration(
+            //       border: OutlineInputBorder(),
+            //       labelText: '이메일을 입력해주세요',
+            //       labelStyle: TextStyle(
+            //         fontSize: 14.sp,
+            //         color: Color(0xffc4c4c4),
+            //       ),
+            //     ),
+            //     onChanged: (value) {
+            //       setState(() {
+            //         _userEmail = value.trim();
+            //       });
+            //     },
+            //   ),
+            // ),
+            // Container(
+            //   color: Colors.white,
+            //   width: ScreenUtil().setWidth(327),
+            //   height: ScreenUtil().setHeight(48),
+            //   child: TextField(
+            //     obscureText: true,
+            //     decoration: InputDecoration(
+            //       border: OutlineInputBorder(),
+            //       labelStyle: TextStyle(
+            //         fontSize: 14.sp,
+            //         color: Color(0xffc4c4c4),
+            //       ),
+            //       labelText: '비밀번호 8~16자, 영문, 숫자, 특수문자',
+            //     ),
+            //     onChanged: (value) {
+            //       setState(() {
+            //         _userPassword = value.trim();
+            //       });
+            //     },
+            //   ),
+            // ),
+            // Padding(
+            //   padding: const EdgeInsets.fromLTRB(0, 20, 0, 3),
+            //   child: Container(
+            //     width: 327.w,
+            //     height: 43.h,
+            //     child: FlatButton(
+            //       onPressed: () async {
+            //         try {
+            //           await AuthController.instance.authentication.signInWithEmailAndPassword(email: _userEmail, password: _userPassword);
+            //         } catch (e) {
+            //           // 스냅바 구현 해야 함.
+            //         }
+            //       },
+            //       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            //       color: Color(0xff74BABC),
+            //       child: Text(
+            //         "로그인",
+            //         style: TextStyle(fontSize: 16.sp, color: Color(0xffffffff), fontWeight: FontWeight.bold),
+            //       ),
+            //     ),
+            //   ),
+            // ),
             Padding(
               padding: const EdgeInsets.fromLTRB(0, 50, 0, 30),
               child: Center(
