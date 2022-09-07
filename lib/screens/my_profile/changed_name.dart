e7aa069c54788219fdd87463becdbb8b11b09fa5
GSD-unity <shhsjk2002@gmail.com>
2022-09-07 16:33:14 +0900

    [Clean] 닉네임 변경 페이지 코드 정리

--- lib/screens/my_profile/changed_name.dart
+++ lib/screens/my_profile/changed_name.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/controller/auth/auth_controller.dart';
-import 'package:flutter_app/main.dart';
-import 'package:flutter_app/screens/my_profile/my_profile.dart';
-import 'package:flutter_app/screens/my_profile/profile.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
+
 
 class ChangedName extends StatefulWidget {
   const ChangedName({Key? key}) : super(key: key);
