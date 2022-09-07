16b161a79bf888b98c6d4f1ef59da668bcc707c6
GSD-unity <shhsjk2002@gmail.com>
2022-09-07 16:29:33 +0900

    [Update] 매니저 검색 기능 (닉네임 검색) 추가

--- lib/screens/manager/manager_search_screen.dart
+++ lib/screens/manager/manager_search_screen.dart
@@ -18,146 +18,269 @@ class _ManagerSearchPageState extends State<ManagerSearchPage> {
   @override
   void dispose() {
     _controller.dispose();
-    // TODO: implement dispose
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      // 여백화면 클릭시 TextField 비활성화
-      onTap: () {
-        // 이용할때 방해될 경우 삭제될 수도 있음
-        FocusScope.of(context).unfocus();
-      },
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          iconTheme: IconThemeData(color: Colors.grey),
-          title: Container(
-            width: ScreenUtil().setWidth(360),
-            child: Center(
-              child: TextField(
-                maxLines: 1,
-                controller: _controller,
-                decoration: InputDecoration(
-                  hintText: '관심분야를 입력해주세요',
-                  suffixIcon: InkWell(
-                      child: Icon(Icons.clear),
-                      onTap: () {
-                        _controller.clear();
-                        setState(() {});
-                      }),
+    return DefaultTabController(
+      length: 2,
+      child: GestureDetector(
+        // 여백화면 클릭시 TextField 비활성화
+        onTap: () {
+          // 이용할때 방해될 경우 삭제될 수도 있음
+          FocusScope.of(context).unfocus();
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.white,
+            iconTheme: IconThemeData(color: Colors.grey),
+            title: Container(
+              width: ScreenUtil().setWidth(360),
+              child: Center(
+                child: TextField(
+                  maxLines: 1,
+                  controller: _controller,
+                  decoration: InputDecoration(
+                    hintText: '관심 있는 리스너를 찾아보세요.',
+                    suffixIcon: InkWell(
+                        child: Icon(Icons.clear),
+                        onTap: () {
+                          _controller.clear();
+                          setState(() {});
+                        }),
+                  ),
                 ),
               ),
             ),
-          ),
-          actions: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 0, 20, 0),
-              child: InkWell(
-                child: Icon(Icons.search),
-                onTap: () {
-                  setState(() {});
-                },
+            bottom: TabBar(
+              labelColor: Colors.black,
+              unselectedLabelColor: Colors.black,
+              //indicatorColor: Colors.greenAccent,
+              indicatorSize: TabBarIndicatorSize.label,
+              indicator: BoxDecoration(
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(10), topRight: Radius.circular(10)),
+                color: Color(0xffe1f3f3),
               ),
-            )
-          ],
-        ),
-        body: Container(
-          width: ScreenUtil().screenWidth,
-          height: ScreenUtil().screenHeight,
-          color: Color(0xffececec),
-          child: StreamBuilder(
-            stream: FirebaseFirestore.instance.collection('user').where('role', isEqualTo: 'manager').snapshots(),
-            builder: (context, AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
-              if (!snapshot.hasData) {
-                return CircularProgressIndicator();
-              }
+              tabs: [
+                Container(
+                    width: ScreenUtil().setWidth(180),
+                    child: Tab(
+                      text: "닉네임",
+                    )),
+                Container(
+                    width: ScreenUtil().setWidth(180),
+                    child: Tab(
+                      text: "관심분야",
+                    )),
+              ],
+            ),
+            actions: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 20, 0),
+                child: InkWell(
+                  child: Icon(Icons.search,),
+                  onTap: () {
+                    setState(() {});
+                  },
+                ),
+              )
+            ],
+          ),
+          body: TabBarView(
+            children: [
+              new Container(              // ########## 닉네임 검색 탭바 Container
+                width: ScreenUtil().screenWidth,
+                height: ScreenUtil().screenHeight,
+                color: Color(0xffececec),
+                child: StreamBuilder(
+                  stream: FirebaseFirestore.instance.collection('user').where('role', isEqualTo: 'manager').snapshots(),
+                  builder: (context, AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+                    if (!snapshot.hasData) {
+                      return CircularProgressIndicator();
+                    }
 
-              final docs = snapshot.data!.docs;
-              if (docs.length == 0) {
-                return Text('null');
-              }
+                    final docs = snapshot.data!.docs;
+                    if (docs.length == 0) {
+                      return Text('null');
+                    }
 
-              final List allDataLike = docs.where((doc) => doc.data()['profile']['like'].contains(_controller.text)).toList();
+                    final List allDataTitle = docs.where((doc) => doc.data()['profile']['title'].contains(_controller.text)).toList();
 
-              return ListView.builder(
-                shrinkWrap: true,
-                itemCount: allDataLike.length,
-                itemBuilder: (context, index) {
-                  // 관심분야 글자수 제한 위한 변수
-                  int likelength = allDataLike[index]['profile']['like'].length;
+                    return ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: allDataTitle.length,
+                      itemBuilder: (context, index) {
+                        // 관심분야 글자수 제한 위한 변수
+                        int likelength = allDataTitle[index]['profile']['title'].length;
 
-                  return InkWell(
-                    onTap: () {
-                      Get.to(() => ManagerDetailPage(allDataLike[index]['userUID']));
-                    },
-                    child: Container(
-                      // 각 리스트 전체 Container
-                      height: ScreenUtil().setHeight(100), // 리스트 전체크기
-                      width: ScreenUtil().screenWidth,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        border: Border.all(width: 0.5, color: Colors.grey),
-                      ),
-                      child: Center(
-                        child: Container(
-                          // 리스트 컨텐츠 가운데 조정 Container
-                          //color: Colors.greenAccent,
-                          width: ScreenUtil().setWidth(360),
-                          height: ScreenUtil().setHeight(80),
-                          child: Row(
-                            children: [
-                              Container(
-                                height: ScreenUtil().setHeight(70),
-                                width: ScreenUtil().setWidth(70),
-                                decoration: BoxDecoration(
-                                    image: DecorationImage(image: NetworkImage(allDataLike[index]['profile']['imageUrl']), fit: BoxFit.cover),
-                                    shape: BoxShape.circle),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(left: 8.0),
-                                child: Container(
-                                  width: ScreenUtil().setWidth(280),
-                                  //color: Colors.green,
-                                  child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      Row(
-                                        mainAxisAlignment: MainAxisAlignment.start,
-                                        children: [
-                                          Text(
-                                            " " + allDataLike[index]['profile']['title'],
-                                            style: TextStyle(fontSize: 20.sp, fontWeight: FontWeight.bold, color: Color(0xff241332)),
-                                          ),
-                                        ],
+                        return InkWell(
+                          onTap: () {
+                            Get.to(() => ManagerDetailPage(allDataTitle[index]['userUID']));
+                          },
+                          child: Container(
+                            // 각 리스트 전체 Container
+                            height: ScreenUtil().setHeight(100), // 리스트 전체크기
+                            width: ScreenUtil().screenWidth,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              border: Border.all(width: 0.5, color: Colors.grey),
+                            ),
+                            child: Center(
+                              child: Container(
+                                // 리스트 컨텐츠 가운데 조정 Container
+                                //color: Colors.greenAccent,
+                                width: ScreenUtil().setWidth(360),
+                                height: ScreenUtil().setHeight(80),
+                                child: Row(
+                                  children: [
+                                    Container(
+                                      height: ScreenUtil().setHeight(70),
+                                      width: ScreenUtil().setWidth(70),
+                                      decoration: BoxDecoration(
+                                          image: DecorationImage(image: NetworkImage(allDataTitle[index]['profile']['imageUrl']), fit: BoxFit.cover),
+                                          shape: BoxShape.circle),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.only(left: 8.0),
+                                      child: Container(
+                                        width: ScreenUtil().setWidth(280),
+                                        //color: Colors.green,
+                                        child: Column(
+                                          mainAxisAlignment: MainAxisAlignment.center,
+                                          children: [
+                                            Row(
+                                              mainAxisAlignment: MainAxisAlignment.start,
+                                              children: [
+                                                Text(
+                                                  " " + allDataTitle[index]['profile']['title'],
+                                                  style: TextStyle(fontSize: 20.sp, fontWeight: FontWeight.bold, color: Color(0xff241332)),
+                                                ),
+                                              ],
+                                            ),
+                                            Row(
+                                              mainAxisAlignment: MainAxisAlignment.start,
+                                              children: [
+                                                likelength > 16 == true
+                                                    ? // like 글자수 제한 16
+                                                Text(" " + " 관심분야 - ${allDataTitle[index]['profile']['like'].substring(0, 16)}",
+                                                    style: TextStyle(fontSize: 15.sp, fontWeight: FontWeight.bold, color: Color(0xff737373)))
+                                                    : Text(" " + " 관심분야 - ${allDataTitle[index]['profile']['like']}",
+                                                    style: TextStyle(fontSize: 15.sp, fontWeight: FontWeight.bold, color: Color(0xff737373))),
+                                              ],
+                                            ),
+                                          ],
+                                        ),
                                       ),
-                                      Row(
-                                        mainAxisAlignment: MainAxisAlignment.start,
-                                        children: [
-                                          likelength > 16 == true
-                                              ? // like 글자수 제한 16
-                                              Text(" " + " 관심분야 - ${allDataLike[index]['profile']['like'].substring(0, 16)}",
-                                                  style: TextStyle(fontSize: 15.sp, fontWeight: FontWeight.bold, color: Color(0xff737373)))
-                                              : Text(" " + " 관심분야 - ${allDataLike[index]['profile']['like']}",
-                                                  style: TextStyle(fontSize: 15.sp, fontWeight: FontWeight.bold, color: Color(0xff737373))),
-                                        ],
-                                      ),
-                                    ],
-                                  ),
+                                    ),
+                                  ],
                                 ),
                               ),
-                            ],
+                            ),
                           ),
-                        ),
-                      ),
-                    ),
-                  );
-                },
-              );
-            },
-          ),
+                        );
+                      },
+                    );
+                  },
+                ),
+              ),
+              new Container(            // ########## 관심 분야 검색 탭바 Container
+                width: ScreenUtil().screenWidth,
+                height: ScreenUtil().screenHeight,
+                color: Color(0xffececec),
+                child: StreamBuilder(
+                  stream: FirebaseFirestore.instance.collection('user').where('role', isEqualTo: 'manager').snapshots(),
+                  builder: (context, AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+                    if (!snapshot.hasData) {
+                      return CircularProgressIndicator();
+                    }
+
+                    final docs = snapshot.data!.docs;
+                    if (docs.length == 0) {
+                      return Text('null');
+                    }
+
+                    final List allDataLike = docs.where((doc) => doc.data()['profile']['like'].contains(_controller.text)).toList();
+
+                    return ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: allDataLike.length,
+                      itemBuilder: (context, index) {
+                        // 관심분야 글자수 제한 위한 변수
+                        int likelength = allDataLike[index]['profile']['like'].length;
+
+                        return InkWell(
+                          onTap: () {
+                            Get.to(() => ManagerDetailPage(allDataLike[index]['userUID']));
+                          },
+                          child: Container(
+                            // 각 리스트 전체 Container
+                            height: ScreenUtil().setHeight(100), // 리스트 전체크기
+                            width: ScreenUtil().screenWidth,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              border: Border.all(width: 0.5, color: Colors.grey),
+                            ),
+                            child: Center(
+                              child: Container(
+                                // 리스트 컨텐츠 가운데 조정 Container
+                                //color: Colors.greenAccent,
+                                width: ScreenUtil().setWidth(360),
+                                height: ScreenUtil().setHeight(80),
+                                child: Row(
+                                  children: [
+                                    Container(
+                                      height: ScreenUtil().setHeight(70),
+                                      width: ScreenUtil().setWidth(70),
+                                      decoration: BoxDecoration(
+                                          image: DecorationImage(image: NetworkImage(allDataLike[index]['profile']['imageUrl']), fit: BoxFit.cover),
+                                          shape: BoxShape.circle),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.only(left: 8.0),
+                                      child: Container(
+                                        width: ScreenUtil().setWidth(280),
+                                        //color: Colors.green,
+                                        child: Column(
+                                          mainAxisAlignment: MainAxisAlignment.center,
+                                          children: [
+                                            Row(
+                                              mainAxisAlignment: MainAxisAlignment.start,
+                                              children: [
+                                                Text(
+                                                  " " + allDataLike[index]['profile']['title'],
+                                                  style: TextStyle(fontSize: 20.sp, fontWeight: FontWeight.bold, color: Color(0xff241332)),
+                                                ),
+                                              ],
+                                            ),
+                                            Row(
+                                              mainAxisAlignment: MainAxisAlignment.start,
+                                              children: [
+                                                likelength > 16 == true
+                                                    ? // like 글자수 제한 16
+                                                Text(" " + " 관심분야 - ${allDataLike[index]['profile']['like'].substring(0, 16)}",
+                                                    style: TextStyle(fontSize: 15.sp, fontWeight: FontWeight.bold, color: Color(0xff737373)))
+                                                    : Text(" " + " 관심분야 - ${allDataLike[index]['profile']['like']}",
+                                                    style: TextStyle(fontSize: 15.sp, fontWeight: FontWeight.bold, color: Color(0xff737373))),
+                                              ],
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  },
+                ),
+              ),
+            ],
+          )
         ),
       ),
     );
