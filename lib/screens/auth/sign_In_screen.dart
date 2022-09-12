8ce62dd73816d65df5755eaa61046ae792701806
GSD-unity <shhsjk2002@gmail.com>
2022-09-12 16:27:16 +0900

    [Update] 매니저 로그인 화면 분리/ 로고 두번 클릭하면 매니저 로그인 화면으로 넘어감.

--- lib/screens/auth/sign_In_screen.dart
+++ lib/screens/auth/sign_In_screen.dart
@@ -1,7 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/controller/auth/auth_controller.dart';
+import 'package:flutter_app/screens/auth/superlogin.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class SignInScreen extends StatefulWidget {
@@ -31,73 +34,15 @@ class _SignInScreenState extends State<SignInScreen> {
         height: ScreenUtil().screenHeight,
         child: Column(
           children: [
-            Container(
-              color: Colors.white,
-              width: ScreenUtil().setWidth(327),
-              height: ScreenUtil().setHeight(48),
-              child: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: '이메일을 입력해주세요',
-                  labelStyle: TextStyle(
-                    fontSize: 14.sp,
-                    color: Color(0xffc4c4c4),
-                  ),
-                ),
-                onChanged: (value) {
-                  setState(() {
-                    _userEmail = value.trim();
-                  });
-                },
-              ),
-            ),
-            Container(
-              color: Colors.white,
-              width: ScreenUtil().setWidth(327),
-              height: ScreenUtil().setHeight(48),
-              child: TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelStyle: TextStyle(
-                    fontSize: 14.sp,
-                    color: Color(0xffc4c4c4),
-                  ),
-                  labelText: '비밀번호 8~16자, 영문, 숫자, 특수문자',
-                ),
-                onChanged: (value) {
-                  setState(() {
-                    _userPassword = value.trim();
-                  });
-                },
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 20, 0, 3),
-              child: Container(
-                width: 327.w,
-                height: 43.h,
-                child: MaterialButton(
-                  onPressed: () async {
-                    try {
-                      await AuthController.instance.authentication.signInWithEmailAndPassword(email: _userEmail, password: _userPassword);
-                    } catch (e) {
-                      // 스냅바 구현 해야 함.
-                    }
-                  },
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                  color: Color(0xff74BABC),
-                  child: Text(
-                    "로그인",
-                    style: TextStyle(fontSize: 16.sp, color: Color(0xffffffff), fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ),
-            ),
+
             Padding(
               padding: const EdgeInsets.fromLTRB(0, 50, 0, 30),
               child: Center(
-                child: Text("나와산책", style: TextStyle(color: Color(0xff000000), fontSize: 32.sp, fontWeight: FontWeight.bold)),
+                child: GestureDetector(
+                  onDoubleTap: (){
+                    Get.to(()=>SuperLogIn());
+                  },
+                    child: Text("나와산책", style: TextStyle( color: Color(0xff324755), fontSize: 32.sp, fontWeight: FontWeight.bold))),
               ),
             ),
             Flexible(
