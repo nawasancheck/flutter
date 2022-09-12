f7ed8a18f800aa28511b1ee3a1d858ee287aaf2b
GSD-unity <shhsjk2002@gmail.com>
2022-09-12 16:38:16 +0900

    [Insert] 매니저로 로그인 페이지 따로 분리 / 로그인 페이지에서 로고 2번 탭하면 이동 됨

--- lib/screens/auth/sign_In_screen.dart
+++ lib/screens/auth/sign_In_screen.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/controller/auth/auth_controller.dart';
-import 'package:flutter_app/screens/auth/superlogin.dart';
+import 'package:flutter_app/screens/auth/super_login.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
