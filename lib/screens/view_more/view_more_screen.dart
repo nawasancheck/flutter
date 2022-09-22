88752aa4f2d9aa506f0585a0550da8beb11295ff
GSD-unity <shhsjk2002@gmail.com>
2022-09-22 16:17:17 +0900

    [Fix] 커밋 초기화,

--- lib/screens/view_more/view_more_screen.dart
+++ lib/screens/view_more/view_more_screen.dart
@@ -1,15 +1,16 @@
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_app/screens/my_profile/customer_service.dart';
-import 'package:flutter_app/screens/my_profile/frequently_asked_questions.dart';
-import 'package:flutter_app/screens/my_profile/listener_profile.dart';
-import 'package:flutter_app/screens/my_profile/my_profile.dart';
-import 'package:flutter_app/screens/my_profile/notice_page.dart';
-import 'package:flutter_app/screens/wish_list/wish_list.dart';
+import 'package:flutter_app/screens/view_more/frequently_asked_questions/frequently_asked_questions.dart';
+import 'package:flutter_app/screens/view_more/listener_profile/listener_profile.dart';
+import 'package:flutter_app/screens/view_more/notice/notice_page.dart';
+import 'package:flutter_app/screens/view_more/profile/my_profile.dart';
+import 'package:flutter_app/screens/view_more/service_center/customer_service.dart';
+import 'package:flutter_app/screens/view_more/support_listener/support_listener.dart';
+import 'package:flutter_app/screens/view_more/wish_list/wish_list.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class Profile extends StatelessWidget {
-  const Profile({Key? key}) : super(key: key);
+class ViewMoreScreen extends StatelessWidget {
+  const ViewMoreScreen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -90,7 +91,7 @@ class Profile extends StatelessWidget {
                       ),
                       title: Transform.translate(
                           offset: const Offset(-15, 0),
-                          child: Text('리스너 프로필관리', style: TextStyle(fontSize: 18.sp, color: const Color(0xff909090), fontWeight: FontWeight.bold)))),
+                          child: Text('리스너 프로필', style: TextStyle(fontSize: 18.sp, color: const Color(0xff909090), fontWeight: FontWeight.bold)))),
                   Container(
                     width: ScreenUtil().screenWidth,
                     height: ScreenUtil().setHeight(40),
@@ -195,6 +196,25 @@ class Profile extends StatelessWidget {
                     title: Transform.translate(
                         offset: const Offset(-15, 0),
                         child: Text('자주묻는 질문', style: TextStyle(fontSize: 18.sp, color: const Color(0xff909090), fontWeight: FontWeight.bold))),
+                  ),
+                  ListTile(
+                    tileColor: const Color(0xffffffff),
+                    onTap: () {
+                      Navigator.of(context, rootNavigator: true).push(MaterialPageRoute(builder: (_) => const SupportListener()));
+                    },
+                    leading: const Icon(
+                      EvaIcons.emailOutline,
+                      size: 30,
+                      color: Color(0xff878787),
+                    ),
+                    trailing: const Icon(
+                        EvaIcons.arrowIosForward,
+                        size: 25,
+                        color: Color(0xff909090)
+                    ),
+                    title: Transform.translate(
+                        offset: const Offset(-15, 0),
+                        child: Text('리스너 지원하기', style: TextStyle(fontSize: 18.sp, color: const Color(0xff909090), fontWeight: FontWeight.bold))),
                   ),
                 ],
               ).toList(),
